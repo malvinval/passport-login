f690a7678fdcb000d27d35b71ac6dd9c15e1ebcc
malvinval <malvinval09@gmail.com>
2023-12-02 10:49:09 +0700

    add some comment

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const routes = require("./routes/web.js");
 // we have to set our view engine (in this case, ejs.)
 app.set("view-engine", "ejs");
 
+// handle requests with registered routes
 app.get(function() { return routes; });
 
+// deploy app at port 8000
 app.listen(8000);

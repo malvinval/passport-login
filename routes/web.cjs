bca9e1fb434506a2910a4dbfe5d9b663f84b9670
malvinval <malvinval09@gmail.com>
2023-12-02 11:14:31 +0700

    update login and register page

--- routes/web.cjs
+++ routes/web.cjs
@@ -17,3 +17,7 @@ app.get("/login", function(req, res) {
 app.get("/register", function(req, res) {
     res.render("register.ejs");
 });
+
+app.post("/register", function(req, res) {
+    res.render("register.ejs");
+});
